70a83bc57d2116a7255760ce4358c138f69b0640
yoshimi <yoshi3923@icloud.com>
2023-02-17 14:57:53 +0900

    fix
    fix line 36, 37

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ model = create_model("Unet_2020-10-30")
 
 model.eval()
 image = load_rgb("343.JPG")
-plt.imshow(image)
-plt.show()
+#plt.imshow(image)
+#plt.show()
 
 transform = albu.Compose([albu.Normalize(p=1)], p=1)
 padded_image, pads = pad(image, factor=32, border=cv2.BORDER_CONSTANT)
@@ -25,12 +25,13 @@ with torch.no_grad():
 mask = (prediction > 0).cpu().numpy().astype(np.uint8)
 mask = unpad(mask, pads)
 
-plt.imshow(mask)
-plt.show()
+#plt.imshow(mask)
+#plt.show()
 
 dst = cv2.addWeighted(image, 1, (cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB) * (0, 255, 0)).astype(np.uint8), 0.5, 0)
 
-plt.imshow(dst)
-plt.show()
+#plt.imshow(dst)
+#plt.show()
 
-imshow(np.hstack([image, cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB) * 255, dst]))
+plt.imshow(np.hstack([image, cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB) * 255, dst]))
+plt.show()
